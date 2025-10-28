3573ff40efdeacf256b618b7cfba1b12324d9806
Mansur <mdadashev13@gmail.com>
2025-10-28 21:17:01 +0400

    Libary 2

---
+++ Libary 2/src/test/java/com/libary/dea/LibaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libary.dea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
